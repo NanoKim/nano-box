ed572ce5f59240e6e06ab3dc6a385fc22dc6951e
Jaeyeong Kim <kjyyy7341@gmail.com>
2026-08-16 02:00:44 +0900

    feat: update vite.config
    feat: update vite.config

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,14 @@
-export * from './src/index.js'
-export {}
-import NanoBox from './src/index.js'
-export default NanoBox
-export * from './src/index.js'
-export {}
+import { App } from 'vue';
+import { NanoInput } from './components/input';
+import { NanoMenu, NanoMenuItem, NanoSubMenu } from './components/menu';
+import { NanoTooltip } from './components/tooltip';
+export { NanoInput };
+export { NanoContainer, NanoHeader, NanoAside, NanoMain, NanoFooter } from './components/layout';
+export { NanoMenu, NanoMenuItem, NanoSubMenu };
+export { NanoTooltip };
+export { default as NanoLayout } from './components/layout';
+export { useThemeTransition } from './composables/theme';
+declare const _default: {
+    install(app: App): void;
+};
+export default _default;
--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,7 @@ export default defineConfig({
     vue(),
     dts({
       insertTypesEntry: true,
+      include: ['src/**/*.ts', 'src/**/*.d.ts', 'src/**/*.vue'],
     }),
   ],
   build: {
